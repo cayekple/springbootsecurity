79651f1fb36c09704d83a16401d166d57b3e7f84
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2021-06-26 12:39:54 +0000

    username and password filter

--- src/main/java/com/cayekple/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/cayekple/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -10,10 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.concurrent.TimeUnit;
 
 import static com.cayekple.springbootsecurity.security.ApplicationUserRole.*;
 
@@ -34,39 +33,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable() // for non-browser base application
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
-//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and() // for browser base application
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated();
-                // form authentication;
-//                .and()
-////                .httpBasic() // basic authentication
-//                .formLogin()
-//                .loginPage("/login")
-//                    .permitAll()
-//                    .defaultSuccessUrl("/courses", true)
-//                    .passwordParameter("password")
-//                    .usernameParameter("username")
-//                .and()
-//                .rememberMe()
-//                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-//                    .key("somethingverysecured")
-//                    .rememberMeParameter("remember-me")
-//                .and()
-//                .logout()
-//                    .logoutUrl("/logout")
-//                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-//                    .clearAuthentication(true)
-//                    .invalidateHttpSession(true)
-//                    .deleteCookies("JSESSIONID", "remember-me")
-//                    .logoutSuccessUrl("/login");
     }
 
     @Override
@@ -82,34 +57,4 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
-//    @Override
-//    @Bean
-//    public UserDetailsService userDetailsServiceBean() throws Exception {
-//        UserDetails annaSmithUser = User.builder()
-//                .username("annasmith")
-//                .password(passwordEncoder.encode("pass"))
-////                .roles(STUDENT.name())
-//                .authorities(STUDENT.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails lindaUser = User.builder()
-//                .username("linda")
-//                .password(passwordEncoder.encode("pass"))
-////                .roles(ADMIN.name())
-//                .authorities(ADMIN.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails tomUser = User.builder()
-//                .username("tom")
-//                .password(passwordEncoder.encode("pass"))
-////                .roles(ADMINTRAINEE.name())
-//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(
-//                annaSmithUser,
-//                lindaUser,
-//                tomUser
-//        );
-//    }
 }
